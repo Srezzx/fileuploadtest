4139a609c450d7dc570abd691265856b164e57d7
Srezzx <srieshimportant@gmail.com>
2021-04-16 15:05:52 +0530

    name added

--- route/user.js
+++ route/user.js
@@ -1,17 +1,19 @@
-const router = require('express').Router();
-const cloudinary = require('../utils/cloudinary');
-const upload = require('../utils/multer');
+const router = require("express").Router();
+const cloudinary = require("../utils/cloudinary");
+const upload = require("../utils/multer");
 
-router.post('/',upload.single('image'),async(req,res) => {
-    try{
-        const result = await cloudinary.v2.uploader.upload(req.file.path);
-        console.log(result);
-        res.json(result);
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
+router.post("/", upload.single("image"), async (req, res) => {
+  try {
+
+    const result = await cloudinary.v2.uploader.upload(req.file.path, {
+      use_filename: true,
+      public_id:req.body.id
+    });
+    console.log(result);
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
